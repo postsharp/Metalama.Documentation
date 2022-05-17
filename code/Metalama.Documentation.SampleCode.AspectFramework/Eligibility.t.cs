f41271733fc1929f252dfbafa62e9b7aa4639610
Gael Fraiteur <gael@postsharp.net>
2022-05-17 08:24:57 +0200

    Metalama update.

--- code/Metalama.Documentation.SampleCode.AspectFramework/Eligibility.t.cs
+++ code/Metalama.Documentation.SampleCode.AspectFramework/Eligibility.t.cs
@@ -1,1 +1,1 @@
-// Error LAMA0037 on `Log`: `The aspect 'Log' cannot be applied to 'SomeClass.StaticMethod()' because 'SomeClass.StaticMethod()' must be non-static.`
+// Error LAMA0037 on `Log`: `The aspect 'Log' cannot be applied to 'SomeClass.StaticMethod()' because 'SomeClass.StaticMethod()' must be a non-static method.`
