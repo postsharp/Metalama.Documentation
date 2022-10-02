a6207bb48c96e0670c73347f4a314bd14604878d
Gael Fraiteur <gael@postsharp.net>
2022-10-02 15:42:51 +0200

    Syntax serializers.

---
+++ code/Metalama.Documentation.SampleCode.AspectFramework/EnumerateMethodInfos.Aspect.cs
@@ -0,0 +1,27 @@
+﻿using Metalama.Framework.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doc.EnumerateMethodInfos
+{
+    internal class EnumerateMethodAspect : TypeAspect
+    {
+        [Introduce]
+        public IReadOnlyList<MethodInfo> GetMethods()
+        {
+            var methods = new List<MethodInfo>();
+
+            foreach ( var method in meta.Target.Type.Methods )
+            {
+                methods.Add( method.ToMethodInfo() );
+            }
+
+            return methods;
+
+        }
+    }
+}
---
+++ code/Metalama.Documentation.SampleCode.AspectFramework/EnumerateMethodInfos.cs
@@ -0,0 +1,9 @@
+﻿namespace Doc.EnumerateMethodInfos
+{
+    [EnumerateMethodAspect]
+    internal class Foo
+    {
+        private void Method1() { }
+        private void Method2( int x, string y) { }
+    }
+}
