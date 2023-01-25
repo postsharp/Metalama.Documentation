b5d9c63bdf275ff225ada214a6914d90b74eae7c
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-01-26 01:07:48 +0530

    Introducing a content folder
    - content
      |_________ Conceptual
      |_________ Getting Started

---
+++ update.cmd
@@ -0,0 +1,1 @@
+C:\Users\Admin\Documents\Github\Metalama.Documentation\docfx\packages\docfx.console.2.59.0\tools\docfx.exe C:\Users\Admin\Documents\Github\Metalama.Documentation\docfx\docfx.json build
