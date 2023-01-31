6636ed5a0d8397601665c8d1a31b8a1fc0bc1c64
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-01-31 14:11:50 +0530

    code for "ThrowOnCall" aspect

---
+++ code/Metalama.Documentation.SampleCode.AspectFramework/ThrowOnCall.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using System;
+
+namespace Doc.ThrowOnCall
+{
+    public class Foo
+    {
+        [ThrowOnCall]
+        private static void OldImplementation()
+        {
+            Console.WriteLine("This is the older implementation");
+        }
+		public static void Main(string[] args)
+		{
+			OldImplementation();
+		}
+    }
+}
