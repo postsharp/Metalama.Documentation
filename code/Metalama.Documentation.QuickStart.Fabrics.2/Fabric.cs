6fc20a44d8a2ce15967b682609b19b49a786e243
GaelFraiteur <gael@postsharp.net>
2023-02-07 16:35:46 +0100

    Adding skeleton for quick start projects.

---
+++ code/Metalama.Documentation.QuickStart.Fabrics.2/Fabric.cs
@@ -0,0 +1,26 @@
+﻿using Metalama.Documentation.QuickStart;
+using Metalama.Framework.Fabrics;
+using Metalama.Framework.Code;
+
+internal partial class Fabric : ProjectFabric
+{
+    public override void AmendProject( IProjectAmender project )
+    {
+        this.AddLogging( project );
+        this.AddNotifyPropertyChanged( project );
+    }
+    
+    private void AddNotifyPropertyChanged( IProjectAmender project )
+    {
+        // Add to namespace.
+        project.With( p => p.GlobalNamespace.GetDescendant( "My.Namsepsace" ).DescendantsAndSelf().SelectMany( ns=> ns.Types ) ).AddAspect(...);
+
+        // Add to any derived type.
+        project.With( p => p.GetDerivedTypes( typeof( BaseClass ) ) ).AddAspectIfEligible<LogAttribute>();
+    }
+}
+
+class BaseClass
+{
+
+}
---
+++ code/Metalama.Documentation.QuickStart.Fabrics/Fabric.cs
@@ -0,0 +1,21 @@
+﻿using Metalama.Documentation.QuickStart;
+using Metalama.Framework.Code;
+using Metalama.Framework.Fabrics;
+
+internal class Fabric : ProjectFabric
+{
+    public override void AmendProject( IProjectAmender project )
+    {
+        //Locating all types 
+        var allTypes = project.Outbound.SelectMany
+                        ( p => p.Types );
+
+        //Finding all public methods from all types
+        var allPublicMethods = allTypes
+                                .SelectMany( t => t.Methods )
+                                .Where( z => z.Accessibility == Accessibility.Public );
+
+        //Adding Log aspect 
+        allPublicMethods.AddAspectIfEligible<LogAttribute>();
+    }
+}
