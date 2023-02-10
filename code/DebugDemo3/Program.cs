c7ec8328b16db4ef3ddd305508413f632e2e91d5
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-02-10 14:15:00 +0530

    Missed files

---
+++ code/DebugDemo3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Metalama.Documentation.QuickStart;
+
+public class Demo
+{
+    public static void Main(string[] args)
+    {
+        string detailXML = GetCustomerDetailsXML("CUST001");
+    }
+
+    [Retry]
+    [Log]
+    public static string GetCustomerDetailsXML(string customerID)
+    {
+        //TODO
+        return string.Empty;
+    }
+}
