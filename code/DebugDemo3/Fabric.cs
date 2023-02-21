98befd007047390f2d5da3e9c9b9063a526f3a58
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-02-21 13:07:30 +0530

    Fixing the code for one fabric case

---
+++ code/DebugDemo3/Fabric.cs
@@ -0,0 +1,30 @@
+﻿using Metalama.Documentation.QuickStart;
+using Metalama.Framework.Fabrics;
+using Metalama.Framework.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugDemo3
+{
+    public class Fabric : ProjectFabric
+    {
+        public override void AmendProject(IProjectAmender project)
+        {
+
+            //Adding Log attribute to all mehtods of all types 
+            //that are available inside "Outer.Inner" namespace 
+
+
+            project.Outbound.Select(t => t.GlobalNamespace)
+                                    .Where(z => z.Name == "Outer.Inner")
+                                    .SelectMany(ns => ns.DescendantsAndSelf())
+                                    .SelectMany(ns => ns.Types)
+                                    .SelectMany(currentType => currentType.Methods)
+                                    .AddAspectIfEligible<LogAttribute>();
+            
+        }
+    }
+}
