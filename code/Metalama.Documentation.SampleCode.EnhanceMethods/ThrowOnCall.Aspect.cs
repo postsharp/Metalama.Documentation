97d2eb3c3d978fbbf0a34a16e81f09bebac84a4d
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-02-24 17:49:32 +0530

    aspect and call

---
+++ code/Metalama.Documentation.SampleCode.EnhanceMethods/ThrowOnCall.Aspect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Metalama.Framework.Aspects;
+
+namespace Doc.ThrowOnCall
+{
+    public class ThrowOnCallAttribute : OverrideMethodAspect
+    {
+        public override dynamic OverrideMethod()
+        {
+            var currentUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            if (currentUser == "gael")
+            {
+                return meta.Proceed();
+            }
+            else
+            {
+                throw new NotSupportedException($"This method can only be called by the current user : {currentUser}");
+            }
+        }
+    }
+}
