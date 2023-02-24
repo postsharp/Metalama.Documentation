e5643d1f0cbe915b80d01e4e5f5be97971a0df44
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-02-24 13:14:58 +0530

    EnhanceMethods proj files

---
+++ code/Metalama.Documentation.SampleCode.EnhanceMethods/_Runner.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Testing.AspectTesting;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace Metalama.Documentation.SampleCode.CompileTimeTesting
+{
+    public class _Runner : AspectTestClass
+    {
+        public _Runner( ITestOutputHelper logger ) : base( logger ) { }
+
+        [Theory]
+        [CurrentDirectory]
+        public Task Test( string f ) => this.RunTestAsync( f );
+    }
+}
