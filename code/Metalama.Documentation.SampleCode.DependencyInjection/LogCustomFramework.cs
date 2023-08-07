ee5cf640810f787a9bfe92d51d170a2b0f42bef0
Petr Onderka <petro@postsharp.net>
2023-08-07 10:46:37 +0200

    Fixed race condition in console output

--- code/Metalama.Documentation.SampleCode.DependencyInjection/LogCustomFramework.cs
+++ code/Metalama.Documentation.SampleCode.DependencyInjection/LogCustomFramework.cs
@@ -1,7 +1,10 @@
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
+
+#if METALAMA
+using Microsoft.Extensions.Logging;
+#endif
 
 namespace Doc.LogCustomFramework
 {
@@ -11,7 +14,9 @@ namespace Doc.LogCustomFramework
         [Log]
         protected override Task ExecuteAsync( CancellationToken stoppingToken )
         {
-            Console.WriteLine( "Hello, world." );
+#if METALAMA
+            _logger.LogInformation( "Hello, world." );
+#endif
             return Task.CompletedTask;
         }
     }
