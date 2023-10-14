2a8822332f588197de5833c68710dc2a2f1613cc
GaelFraiteur <gael@postsharp.net>
2023-10-14 14:22:06 +0200

    Adding TestLoop.ps1

--- code/TestLoop.ps1
+++ code/TestLoop.ps1
@@ -1,15 +1,12 @@
 for ($i = 1; $i -le 100; $i++) {
     
     $currentTime = Get-Date
-    Write-Host "Running iteration $i at $currentTime"
+    Write-Host "=================================================== Running iteration $i at $currentTime ===================================================================="
     
-    dotnet test C:\src\Metalama.Documentation\code\Metalama.Documentation.Snippets.TestBased.sln
+    & dotnet test C:\src\Metalama.Documentation\code\Metalama.Documentation.Snippets.TestBased.sln
 
-    $process = Start-Process -FilePath "dotnet" -ArgumentList "test C:\src\Metalama.Documentation\code\Metalama.Documentation.Snippets.TestBased.sln" -Wait -PassThru
-
-    if ($process.ExitCode -ne 0) {
-        Write-Host "Program returned a non-zero value. Exiting loop."
-        break
+    if ($LASTEXITCODE -ne 0) {
+        Throw "dotnet test failed."
     }
 
 }
