bda40596f6225fab8b1cdbaf7152ecb8ea7cd5aa
Antonín Procházka <antonin@postsharp.net>
2024-08-21 14:20:33 +0200

    DFM extensions removed.

--- code/Metalama.Documentation.DfmExtensions/DotNetVersions.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Immutable;
-
-namespace Metalama.Documentation.DfmExtensions;
-
-internal static class DotNetVersions
-{
-    public static ImmutableArray<string> Versions { get; } = ImmutableArray.Create( "net6.0", "net7.0" );
-}
